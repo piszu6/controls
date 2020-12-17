82a05ce530d333cc6662434db60cc5d9322ae9bb
piszu <lukasz@szwedo.info>
2020-12-17 15:40:45 +0100

    Input added

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+
+  @Input() public placeholder: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
